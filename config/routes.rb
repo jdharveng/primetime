528cc880beafbcabe7eaa1942ad476c307d1af8f
Pedro Marzagao <pedromarzagao@gmail.com>
2018-06-04 18:10:18 +0100

    created activities routes, policies and basic css for containers

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get 'activities/destroy'
   devise_for :users
   root to: 'pages#home'
+  resources :activities
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
