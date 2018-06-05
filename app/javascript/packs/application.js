05db10c4ec7b8b49e26690708c3ab3ea9017999a
Andreia Domingues <andreianetface@gmail.com>
2018-06-05 17:22:50 +0100

    sliders working fine and sending correct information, not yet with animation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,18 @@ slide_with_ticks();
 
 const next_button = document.getElementById( 'next-filter' );
 
+document.getElementById("money").style.display = "none";
+
 next_button.onclick = function() {
 
   console.log("It's working")
+
+  var x = document.getElementById("money");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+        document.getElementById("time").style.display = "none";
+    } else {
+        x.style.display = "none";
+    }
+
 }
