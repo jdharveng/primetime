87508704ef95f8dc6c328b3abeb6d0cc74995c34
Pedro Marzagao <pedromarzagao@gmail.com>
2018-06-05 14:48:01 +0100

    added geocoder config and Activity model settings

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -7,4 +7,6 @@ class Activity < ApplicationRecord
   validates :title, presence: true
   validates :duration, presence: true
 
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
