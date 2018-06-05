a63ae2a6ebf1d301b87abe60e7f4b2d71b47258c
jerome dharveng <jerome.dharveng@hotmail.com>
2018-06-05 11:57:19 +0100

    First commit on Controller set up

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,31 +1,62 @@
 class ActivitiesController < ApplicationController
+  before_action :set_activity, only: [:show, :edit, :update, :destroy]
 
-
+  # GET /activities
   def index
-    @activities = Activity.all
+    # @activities = Activity.all
+    @activities = policy_scope(Activity).order(created_at: :desc)
   end
 
+  # GET /activities/1
   def show
-
   end
 
+  # GET /activities/new
   def new
+    @activity = Activity.new
   end
 
+  # POST /activities/
   def create
+    @activity = Activity.new(activity_params)
+    @activity.user_id = current_user.id
+    if @activity.save
+      redirect_to @activity
+    else
+      render :new
+    end
 
-
-    authorize @activity
   end
 
+  # GET /activities/1/edit
   def edit
-  end
-
-  def update
-
     authorize @activity
   end
 
-  def destroy
+  # PATCH /activities/1
+  def update
+    authorize @activity
+    if @activity.update(acitivity_params)
+      redirect_to @activity
+    else
+      render :edit
+    end
   end
+
+  # DELETE /activities/1
+  def destroy
+    authorize @activity
+    @activity.destroy
+  end
+
+  private
+
+  def activity_params
+    params.require(:activity).permit(:title, :description, :duration, :address, :price)
+  end
+
+  def set_activity
+    @activity = Activity.find(params[:id])
+  end
+
 end
--- app/policies/activity_policy.rb
+++ app/policies/activity_policy.rb
@@ -1,7 +1,7 @@
 class ActivityPolicy < ApplicationPolicy
   class Scope < Scope
     def resolve
-      scope
+      scope.all
     end
   end
 
