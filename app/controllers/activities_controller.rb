8a688c182be90070c86bfb5c850df4c90021791c
Andreia Domingues <andreianetface@gmail.com>
2018-06-07 13:12:16 +0100

    changed the need for authenticating user for the show page in the controller, so that fb can fetch title of activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,6 @@
 class ActivitiesController < ApplicationController
   before_action :set_activity, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:show]
   require 'json'
   require 'open-uri'
 
