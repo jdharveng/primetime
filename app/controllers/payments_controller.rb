a66d9fdc070f62fbcc0494ace83bc4f401cb09eb
Pedro Marzagao <pedromarzagao@gmail.com>
2018-06-11 19:32:34 +0100

    fixes in slideout and in payment process

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsController < ApplicationController
    before_action :set_booking, only: [:new, :create]
+   protect_from_forgery with: :null_session
 
   def new
     @activity = @booking.activity
@@ -18,7 +19,7 @@ class PaymentsController < ApplicationController
   charge = Stripe::Charge.create(
     customer:     customer.id,   # You should store this customer id and re-use it.
     amount:       @booking.amount_cents,
-    description:  "Payment for teddy #{@booking.activity} for order #{@booking.id}",
+    description:  "Payment for #{@booking.activity} for order #{@booking.id}",
     currency:     @booking.amount.currency
   )
 
